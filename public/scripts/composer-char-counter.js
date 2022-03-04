7003d28c0514f2a9f3bb30ec4569754993fba164
carmen kuang <carmenkuang87@gmail.com>
2022-03-04 08:35:53 +0000

    eslint code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   
   //let remainingCharsText = document.getElementById("remainingChars");
- $(".new-tweet textarea").on('input', function() {
+  $(".new-tweet textarea").on('input', function() {
     const remainingCharsText = $('#remainingChars')[0];
-    const max_chars =140;
-    const remaining = max_chars - $(this).val().length
+    const max_chars = 140;
+    const remaining = max_chars - $(this).val().length;
     
     remainingCharsText.textContent = remaining;
-    remainingCharsText.style.color= remaining <0 ? "red": "#585858";
+    remainingCharsText.style.color = remaining < 0 ? "red" : "#585858";
   });
 });
